4df7284fa4452246853fd5dbf440a412078060be
Samuel García <sammomx@gmail.com>
2022-06-17 00:55:51 -0500

    Se agregó el uso de un while

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ Programa para conocer el promedio de edad de una cantidad específica de estudia
 
 */
 
-let numero1 = parseInt(prompt("¿Cuántos alumnos son?"));
+let numero1 = parseInt(prompt("Vamos a promediar las edades de la clase ¿Cuántos alumnos son?"));
 let suma = 0;
 
 /*Es un número fijo pero preferí preguntar al usuario la cantidad de alumnos. */
@@ -14,6 +14,21 @@ for (i=0; i<numero1; i++){
     let numero2 = parseInt(prompt("¿Edad del estudiante #"+(i+1)+' : '));
     suma= suma+numero2;
 }
-
 alert("El promedio de edad de los "+numero1+' alumnos es de : '+parseInt(suma/numero1)+' años.');
 
+/*
+Programa para deletrear un nombre o palabra
+ */
+prompt("Instrucciones : \n1- Captura letra por letra tu nombre.\n2- Al finalizar captura 'ESC'.\n...presiona ENTER para iniciar.");
+let letra = prompt("Ingresa la primer letra ('ESC' para finalizar)");
+let cadena = letra;
+let textoConcatenar = " ";
+
+while (textoConcatenar != "ESC") {
+    textoConcatenar = prompt("Ingresa la siguiente letra ('ESC' para finalizar)");
+    if (textoConcatenar != "ESC") {
+        cadena = cadena+textoConcatenar;
+    }    
+}
+
+prompt("El nombre capturado es: "+cadena+"\nENTER para salir.");
